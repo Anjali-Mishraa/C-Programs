e75e48c3d2867e604143ebfda5f8bf8c1e5272c6
Anjali Mishra <66460694+Anjali-Mishraa@users.noreply.github.com>
2020-06-05 19:16:35 +0530

    Created code2.c
    This file can be used for grading purpose in an elementary school.

---
+++ code2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main ()
+{
+  int m;
+  printf ("Enter marks of a student");
+  scanf ("%d",&m);
+  if (m<=100 && m>=85)
+  printf ("Grade A");
+  else if (m<=84 && m>=70)
+  printf ("Grade B");
+  else if (m<=69 && m>=55)
+  printf ("Grade C");
+  else if (m<=54 && m>=40)
+  printf ("Grade D");
+  else 
+  printf ("Grade F");
+  return 0;
+}
